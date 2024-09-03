5e23656eccd98374218dbba00b2d9f4a25f47256
jack <jichengde@gmail.com>
2024-09-03 23:43:51 +0800

    first commit

---
+++ zigup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "usage: zigup.sh master|0.13.0|0.13.0-dev.351+64ef45eb0"
+    exit -1
+fi
+
+case $1 in
+    master)
+        pkg=`curl -s https://ziglang.org/download/index.json | jq '.master."x86_64-linux".tarball' | sed 's/"//g' | awk -F / '{print $NF}'`
+        ;;
+
+    *)
+        pkg="zig-linux-x86_64-$1.tar.xz"
+        ;;
+esac
+
+if [ ! -e ~/.zigup/ ]; then
+    mkdir -p ~/.zigup
+fi
+
+url="https://ziglang.org/builds/$pkg"
+if [ ! -e ~/.zigup/$pkg ]; then
+    wget -O ~/.zigup/$pkg $url
+    tar -xJvf ~/.zigup/$pkg -C ~/.zigup
+fi
+
+echo "You might need to input sudo password..."
+sudo ln -sf ~/.zigup/${pkg%.tar.xz}/zig /usr/local/bin
+
+echo "$(zig version) is ready for you now!"

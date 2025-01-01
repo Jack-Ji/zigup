e5f72af39e28f0abcee27f498009406e5f702238
jack <jichengde@gmail.com>
2025-01-01 19:31:44 +0800

    detect .zigversion

--- zigup.sh
+++ zigup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [ $# -eq 0 ] && [ -e .zigversion ]; then
+    version=`cat .zigversion`
+elif [ $# -ne 1 ]; then
     echo "usage: zigup.sh master|0.13.0|0.13.0-dev.351+64ef45eb0"
     exit -1
+else
+    version=$1
 fi
 
 set -e
 
-if echo $1 | grep -q dev; then
-    pkg=zig-linux-x86_64-$1.tar.xz
+if echo $version | grep -q dev; then
+    pkg=zig-linux-x86_64-$version.tar.xz
     url=https://ziglang.org/builds/$pkg
 else
-    url=`curl -s https://ziglang.org/download/index.json | jq --arg V $1 '.[$V]."x86_64-linux".tarball' | sed 's/"//g'`
+    url=`curl -s https://ziglang.org/download/index.json | jq --arg V $version '.[$V]."x86_64-linux".tarball' | sed 's/"//g'`
     pkg=`echo $url | awk -F / '{print $NF}'`
 fi
 
